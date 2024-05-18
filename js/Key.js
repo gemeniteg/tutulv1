c7f6cb65449092776fcd1854180885264c7d5811
gemeniteg <gemeniteg@gmail.com>
2024-05-18 17:57:33 +0800

    add chains for owlto

--- js/Key.js
+++ js/Key.js
@@ -6,18 +6,35 @@
 
 	var touchStartX = 0;
 	var touchStartY = 0;
+	var isDrawing = false;
+
+	var canvas = document.getElementById("canvas_intro");
+	var ctx = canvas.getContext("2d");
+	
+    function drawPoint(x, y, ctx) {
+        ctx.beginPath();
+        ctx.arc(x, y, 1, 0, 2 * Math.PI);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function clearCanvas(ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 
 	// Event Handling for touchstart
 	var onTouchStart = function (event) {
 		touchStartX = event.touches[0].clientX;
 		touchStartY = event.touches[0].clientY;
-
+		isDrawing = true;
+        var touch = event.touches[0];
+        drawPoint(touch.clientX - canvas.offsetLeft, touch.clientY - canvas.offsetTop, ctx);
 		if (window.STAGE == 4) return;
 		event.stopPropagation();
-		event.preventDefault();
+		
 	}
 
-	var onTouchMove = function(event){
+	var onTouchMove = function (event) {
 		var touchX = event.touches[0].clientX;
 		var touchY = event.touches[0].clientY;
 		var deltaX = touchX - touchStartX;
@@ -26,36 +43,42 @@
 		Key["right"] = false; // Right
 		Key["up"] = false; // Down
 		Key["down"] = false; // Up
-		
+
 		var di = 50
-		if(deltaX > di) {
+		if (deltaX > di) {
 			Key["right"] = true; // Right
-		} else if(deltaX < -di) {
+		} else if (deltaX < -di) {
 			Key["left"] = true; // Left
 		}
 
-		if(deltaY > di) {
+		if (deltaY > di) {
 			Key["down"] = true;  // Down
-		} else if(deltaY < -di) {
+		} else if (deltaY < -di) {
 			Key["up"] = true;  // Up
 		}
+		if (isDrawing) {
+            var touch = event.touches[0];
+            clearCanvas(ctx);
+            drawPoint(touch.clientX - canvas.offsetLeft, touch.clientY - canvas.offsetTop, ctx);
 
-		
+		}
 		if (window.STAGE == 4) return;
 		event.stopPropagation();
-		event.preventDefault();
+		
 	}
 
-	
+
 	// Event Handling for touchend
 	var onTouchEnd = function (event) {
 		Key["left"] = false; // Left
 		Key["right"] = false; // Right
 		Key["up"] = false; // Down
 		Key["down"] = false; // Up
+		isDrawing = false;
+        clearCanvas(ctx);
 		if (window.STAGE == 4) return;
 		event.stopPropagation();
-		event.preventDefault();
+		
 	}
 
 	window.addEventListener("touchstart", onTouchStart, false);
